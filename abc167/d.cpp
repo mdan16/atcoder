5c45805c17974b8b4314ab652e5709380a9211ee
mdan <t.t.mina16@gmail.com>
2020-05-11 22:35:11 +0900

    abc167

--- abc167/d.cpp
+++ abc167/d.cpp
@@ -17,65 +17,34 @@ const ll mod = 1e9+7;
 int dy[] = {0, 0, 1, -1};
 int dx[] = {1, -1, 0, 0};
 
-ll N, K;
-vector<ll> A;
 int loop_start;
 int loop_start_count;
 
-int loop() {
-    vector<bool> arrived(N, false);
-    // check loop start position
-    int now = 1;
-    loop_start_count = 0;
-    while(true) {
-        if (arrived.at(now - 1) == true) {
-            loop_start = now;
-            break;
-        }
-        arrived.at(now - 1) = true;
-        now = A.at(now - 1);
-        loop_start_count++;
-    }
-
-    // check loop length
-    int loop_len = 0;
-    now = 1;
-    bool in_loop = false;
-    while(true) {
-        now = A.at(now - 1);
-        if (now == loop_start) {
-            if (in_loop) {
-                break;
-            } else {
-                in_loop = true;
-            }
-        }
-        if (in_loop) {
-            loop_len++;
-        }
-    }
-    return loop_len;
-}
-
 int main() {
+    int N;
+    ll K;
     cin >> N >> K;
-    A.resize(N);
-    FOR(i, 0, N) {
-        cin >> A.at(i);
-    }
+    vector<int> A(N);
+    REP(i, N) cin >> A.at(i);
 
-    int location = 1;
+    vector<int> s;
+    vector<int> ord(N+1, -1);
 
-    int loop_len = loop();
+    int c = 1;
+    int l = 0;
+    int v = 1;
+    while(ord[v] == -1) {
+        ord[v] = s.size();
+        s.push_back(v);
+        v = A[v-1];
+    }
+    c = s.size() - ord[v];
+    l = ord[v];
 
-    int move_num = (K - loop_start_count) % loop_len;
-    int now = loop_start;
-    if (move_num < 0) {
-        now = 1;
-        move_num = K;
+    if (K < l) cout << s[K] << endl;
+    else {
+        K -= l;
+        K = K % c;
+        cout << s[K + l] << endl;
     }
-    FOR(i, 0, move_num) {
-        now = A.at(now - 1);
-    }
-    cout << now << endl;
 }
