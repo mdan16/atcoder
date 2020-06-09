9d7d1cacde721536c4964aaffd1b97670dd489c4
mdan <t.t.mina16@gmail.com>
2020-06-10 08:13:55 +0900

    abc162

---
+++ abc162/c.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define FOR(i,a,b)  for(ll i=a;(i)<(b);++(i))
+#define RFOR(i,a,b) for(ll i=a;(i)>=(b);--(i))
+#define REP(i,n)    FOR(i,0,n)
+#define RREP(i,n)   RFOR(i,n,0)
+#define ALL(v)      v.begin(), v.end()
+#define UNIQ(v)     sort(ALL(v)); v.erase(unique(ALL(v)), v.end())
+#define BIT(n)      (1LL<<(n))
+#define DEBUG(a)    cerr << #a << " = " << a << endl
+
+const double PI = acos(-1);
+const int inf = 1001001001;
+const int mod = (int)1e9+7;
+//const ll inf = 1e15;
+//const ll mod = 1e9+7;
+
+int dy[] = {0, 0, 1, -1};
+int dx[] = {1, -1, 0, 0};
+
+
+int main() {
+    int K;
+    cin >> K;
+    int ans = 0;
+    FOR (i, 1, K+1) {
+        FOR (j, 1, K+1) {
+            FOR (k, 1, K+1) {
+                ans += gcd(i, gcd(j, k));
+            }
+        }
+    }
+    cout << ans << endl;
+}
